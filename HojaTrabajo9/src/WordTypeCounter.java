a37c5a13c7ac92b88d1bc504254bbd22dc52fd57
diego6425 <con14188@uvg.edu.gt>
2015-10-07 08:11:16 -0600

    5envio

--- HojaTrabajo9/src/WordTypeCounter.java
+++ HojaTrabajo9/src/WordTypeCounter.java
@@ -7,18 +7,18 @@ Autor: Eduardo Castellanos
 Descripción: Programa principal. 
 */
 import java.io.*;
+import javax.swing.*;
 
 class WordTypeCounter {
 	public static void main(String[] args) throws Exception
 	{
-		if(args.length > 1)
-		{
+		
 			// Declaración e inicialización de variables.
 			// el primer parametro indica el nombre del archivo con las definiciones de las palabras
-			File wordFile = new File(args[0]);
+			File wordFile = new File("words.txt");
 			
 			// el segundo parametro indica el nombre del archivo que tiene el texto a analizar
-			File textFile = new File(args[1]);
+			File textFile = new File("text.txt");
 			
 			// el tercer parametro sirve para seleccionar la implementacion que se usara para
 			// guardar el conjunto de palabras. Use el valor 1 para indicar que se empleara
@@ -28,7 +28,7 @@ class WordTypeCounter {
 			//  3 Splay Tree
 			//  4 Hash Table
 			//  5 TreeMap (de java collection framework)
-			int implementacion = Integer.parseInt(args[2]);
+			int implementacion = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese numero del 1 al 5"));
 			
 			BufferedReader wordreader;
 			BufferedReader textreader;
@@ -134,10 +134,6 @@ class WordTypeCounter {
 			{
 				System.out.println("No encuentro los archivos :'( ");
 			}
-		}
-		else
-		{
-			System.out.println("Faltan Parametros.");
-		}
+		
 	}
 }
